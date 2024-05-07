bb547d407acc6198f1003b879c8fb3d734a1f67c
Oleh-Shkuliepov <163303427+Oleh-Shkuliepov@users.noreply.github.com>
2024-05-07 21:18:28 +0300

    add-oleh-heder-hero-js

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+
+    const button_open = document.querySelector('.header-menu-open-btn');
+    const button_close = document.querySelector('.mob-menu-btn');
+    const menu = document.querySelector('.mob-menu');
+
+    button_open.addEventListener('click', () => {
+        menu.classList.add('is-open');
+    });
+    button_close.addEventListener('click', () => {
+        menu.classList.remove('is-open');
+    });
